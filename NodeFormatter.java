ca24a42e0d2037d75542058e6d4d64f54beea81b
vikas91 <chandrashekar.v@husky.neu.edu>
2017-10-25 22:02:16 -0400

    commit

--- NodeFormatter.java
+++ NodeFormatter.java
@@ -1,5 +1,10 @@
 
+// NodeFormatter is an object of any class that implements NodeFormatter
+
+// Interpretation: Formats the nodes in AST to simplify the data structure
+// before comparing
 public interface NodeFormatter {
 
+	// Simplify the given node to the required structure for using in bipartite matching
 	void formatNode(Object obj);
 }
